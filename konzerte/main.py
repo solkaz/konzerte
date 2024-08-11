90b4bf80cfcbd0ca11b939ab80ad3d163d8b2790
Jeff Held <jh.software.developer@gmail.com>
2024-08-11 12:56:03 +0200

    Add templating base

--- konzerte/main.py
+++ konzerte/main.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse(
+        "main.html",
+        {
+            "request": request,
+        },
+    )
